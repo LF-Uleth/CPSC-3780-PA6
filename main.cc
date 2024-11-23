1832f4251c45a8eaa923a1cddb976c6df925d47c
LF-Uleth <144730637+LF-Uleth@users.noreply.github.com>
2024-11-23 11:58:08 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -15,62 +15,50 @@ void send_file(const string& file_path, const string& host, int port) {
     memset(&server_addr_in, 0, sizeof(server_addr_in));
     memset(&server_addr_in6, 0, sizeof(server_addr_in6));
 
-    // Check if the host is IPv4 or IPv6
+    //Check if ipv4 or 6
     struct sockaddr_in sa;
     struct sockaddr_in6 sa6;
     bool is_ipv6 = inet_pton(AF_INET6, host.c_str(), &(sa6.sin6_addr)) != 0;
 
-    if (is_ipv6) {
-        // Create an IPv6 socket
+    if (is_ipv6) { //ipv6
         sockfd = socket(AF_INET6, SOCK_STREAM, 0);
-
-        // Prepare IPv6 address structure
         server_addr_in6.sin6_family = AF_INET6;
         server_addr_in6.sin6_port = htons(port);
         inet_pton(AF_INET6, host.c_str(), &server_addr_in6.sin6_addr);
-    } else {
-        // Create an IPv4 socket
+        
+    } else { //ipv4 
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-        // Prepare IPv4 address structure
         server_addr_in.sin_family = AF_INET;
         server_addr_in.sin_port = htons(port);
         inet_pton(AF_INET, host.c_str(), &server_addr_in.sin_addr);
     }
 
-    // Connect to the server
+    //connect based on ipv4 or 6
     if (is_ipv6) {
-        connect(sockfd, (struct sockaddr *)&server_addr_in6, sizeof(server_addr_in6));
+        connect(sockfd, (struct sockaddr *)&server_addr_in6, sizeof(server_addr_in6)); //6
     } else {
-        connect(sockfd, (struct sockaddr *)&server_addr_in, sizeof(server_addr_in));
+        connect(sockfd, (struct sockaddr *)&server_addr_in, sizeof(server_addr_in)); //4
     }
 
-    // Open the file to send
+    //get file
     ifstream file(file_path, ios::binary);
     char buffer[512];
     file.read(buffer, sizeof(buffer));
     streamsize bytes_read = file.gcount();
 
-    // Send the file data to the server
+    //send file
     send(sockfd, buffer, bytes_read, 0);
 
-    // Close the socket and file
+    //close soceket + file
     close(sockfd);
     file.close();
 }
 
 int main(int argc, char* argv[]) {
-    // Ensure correct number of arguments
-    if (argc != 5 || string(argv[1]) != "-f") {
-        cerr << "Usage: sender -f <data_file> <host> <port>" << endl;
-        return 1;
-    }
-
     string file_path = argv[2];
     string host = argv[3];
     int port = atoi(argv[4]);
-
-    // Call the send_file function to send the file
+    
     send_file(file_path, host, port);
 
     return 0;
